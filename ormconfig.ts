6452c50684dc59ccf47e8355e785d01e7d00c935
MIShuvro <manjarulislam75@gmail.com>
2022-08-15 21:32:02 +0600

    init project

---
+++ ormconfig.ts
@@ -0,0 +1,16 @@
+require("dotenv").confiq({
+    path: process.env.ENV_PATH
+})
+console.log('driver', process.env.DB_DRIVER);
+
+const ormConfig: any = {
+    type: process.env.DB_DRIVER,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    synchronize: false,
+    logging: false,
+}
